81fb15bff913cd16ef5aecb34013ce176225bdc6
Nour Fakih <fakihnourzahraa@gmail.com>
2025-09-15 19:41:35 +0300

    debugging redir

--- tokenization/parsing.c
+++ tokenization/parsing.c
@@ -6,7 +6,7 @@
 /*   By: nour <nour@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 22:53:52 by nour              #+#    #+#             */
-/*   Updated: 2025/09/15 19:19:41 by nour             ###   ########.fr       */
+/*   Updated: 2025/09/15 19:41:16 by nour             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,98 +57,6 @@ t_cmd	*init_cmd(t_shell *shell, t_token *t)
 	return (cmd);
 }
 
-
-// void	parse(t_shell *shell, t_token *token)
-// {
-// 	int		i;
-// 	t_cmd	*cmd;
-
-// 	if (!token || token->type != WORD)
-// 		return ;
-// 	cmd = init_cmd(shell, token);
-// 	cmd->cmd = ft_strdup(token->s);
-// 	cmd->args[0] = ft_strdup(token->s);
-// 	i = 1;
-// 	token = token->next;
-// 	while (token && token->type != T_EOF && token->type != PIPE)
-// 	{
-// 		if(token->type == WORD)
-// 		{
-// 			if (token->quotes == 1)
-// 				cmd->args[i] = ft_strtrim(token->s, "'");
-// 			else if (token->quotes == 2)
-// 				cmd->args[i] = ft_strtrim(token->s, "\"");
-// 			else
-// 				cmd->args[i] = ft_strdup(token->s);
-// 			i++;
-// 			token = token->next;
-// 		}
-// 		else
-// 		{
-// 			fill_r_for_cmd(token, cmd);
-// 			token = token->next;
-// 			if (token && token->type == WORD)
-// 				token = token->next;
-// 		}
-// 	}
-// 	cmd->args[i] = NULL;
-// 	add_cmd(shell, cmd);
-// 	if (token && token->type == PIPE)
-// 	{
-// 		token = token->next;
-// 		parse(shell, token);
-// 	}
-// }
-
-// FIXED: This function now processes redirections for the CURRENT command only
-void	fill_r_for_cmd(t_token *redir_token, t_token *file_token, t_cmd *cmd)
-{
-	t_redir *r;
-
-	if (!redir_token || redir_token->type == T_EOF)
-		return ;
-		
-	r = malloc(sizeof(t_redir));
-	if (!r)
-		return;
-		
-	r->fd = -1;
-	r->s = NULL;
-	r->type = R_EMPTY;
-	r->next = NULL;
-	
-	if (redir_token->type == IN)
-		r->type = R_IN;
-	else if (redir_token->type == OUT)
-		r->type = R_OUT;
-	else if (redir_token->type == APPEND)
-		r->type = R_APPEND;
-	else if (redir_token->type == HEREDOC)
-		r->type = R_HEREDOC;
-	else
-	{
-		free(r);
-		return ;
-	}
-	
-	// Use the file_token parameter instead of redir_token->next
-	if (file_token && file_token->s)
-		r->s = ft_strdup(file_token->s);
-	else
-		r->s = NULL;
-	
-	// Add redirection to THIS command
-	if (!cmd->rd)
-		cmd->rd = r;
-	else
-	{
-		t_redir *curr = cmd->rd;
-		while (curr->next)
-			curr = curr->next;
-		curr->next = r;
-	}
-}
-
 void	parse(t_shell *shell, t_token *token)
 {
 	int		i;
@@ -156,20 +64,14 @@ void	parse(t_shell *shell, t_token *token)
 
 	if (!token || token->type != WORD)
 		return ;
-		
 	cmd = init_cmd(shell, token);
-	if (!cmd)
-		return;
-		
 	cmd->cmd = ft_strdup(token->s);
 	cmd->args[0] = ft_strdup(token->s);
 	i = 1;
 	token = token->next;
-	
-	// FIXED: Process tokens for THIS command only (until PIPE or EOF)
 	while (token && token->type != T_EOF && token->type != PIPE)
 	{
-		if (token->type == WORD)
+		if(token->type == WORD)
 		{
 			if (token->quotes == 1)
 				cmd->args[i] = ft_strtrim(token->s, "'");
@@ -180,43 +82,94 @@ void	parse(t_shell *shell, t_token *token)
 			i++;
 			token = token->next;
 		}
-		else if (token->type == IN || token->type == OUT || 
-				 token->type == APPEND || token->type == HEREDOC)
-		{
-			// FIXED: Store redirection token and get filename token
-			t_token *redir_token = token;
-			token = token->next; // Move to filename token
-			
-			if (token && token->type == WORD)
-			{
-				// Add redirection with both tokens
-				fill_r_for_cmd(redir_token, token, cmd);
-				token = token->next; // Skip filename token
-			}
-			else
-			{
-				// Invalid syntax - redirection without filename
-				// Still need to advance token to avoid infinite loop
-				if (token)
-					token = token->next;
-			}
-		}
 		else
 		{
+			fill_r(token, token->next);
 			token = token->next;
+			if (token && token->type == WORD)
+				token = token->next;
 		}
 	}
-	
 	cmd->args[i] = NULL;
 	add_cmd(shell, cmd);
-	
-	// FIXED: If we hit a PIPE, recursively parse the next command
 	if (token && token->type == PIPE)
 	{
-		token = token->next; // Skip the PIPE token
-		parse(shell, token);  // Parse next command after pipe
+		token = token->next;
+		parse(shell, token);
 	}
+	
 }
+
+// FIXED: This function now processes redirections for the CURRENT command only
+
+// void	parse(t_shell *shell, t_token *token)
+// {
+// 	int		i;
+// 	t_cmd	*cmd;
+
+// 	if (!token || token->type != WORD)
+// 		return ;
+		
+// 	cmd = init_cmd(shell, token);
+// 	if (!cmd)
+// 		return;
+		
+// 	cmd->cmd = ft_strdup(token->s);
+// 	cmd->args[0] = ft_strdup(token->s);
+// 	i = 1;
+// 	token = token->next;
+	
+// 	// FIXED: Process tokens for THIS command only (until PIPE or EOF)
+// 	while (token && token->type != T_EOF && token->type != PIPE)
+// 	{
+// 		if (token->type == WORD)
+// 		{
+// 			if (token->quotes == 1)
+// 				cmd->args[i] = ft_strtrim(token->s, "'");
+// 			else if (token->quotes == 2)
+// 				cmd->args[i] = ft_strtrim(token->s, "\"");
+// 			else
+// 				cmd->args[i] = ft_strdup(token->s);
+// 			i++;
+// 			token = token->next;
+// 		}
+// 		else if (token->type == IN || token->type == OUT || 
+// 				 token->type == APPEND || token->type == HEREDOC)
+// 		{
+// 			// FIXED: Store redirection token and get filename token
+// 			t_token *redir_token = token;
+// 			token = token->next; // Move to filename token
+			
+// 			if (token && token->type == WORD)
+// 			{
+// 				// Add redirection with both tokens
+// 				fill_r_for_cmd(redir_token, token, cmd);
+// 				token = token->next; // Skip filename token
+// 			}
+// 			else
+// 			{
+// 				// Invalid syntax - redirection without filename
+// 				// Still need to advance token to avoid infinite loop
+// 				if (token)
+// 					token = token->next;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			token = token->next;
+// 		}
+// 	}
+	
+// 	cmd->args[i] = NULL;
+// 	add_cmd(shell, cmd);
+	
+// 	// FIXED: If we hit a PIPE, recursively parse the next command
+// 	if (token && token->type == PIPE)
+// 	{
+// 		token = token->next; // Skip the PIPE token
+// 		parse(shell, token);  // Parse next command after pipe
+// 	}
+// }
 
 //extra token.next so it skips filename
 
